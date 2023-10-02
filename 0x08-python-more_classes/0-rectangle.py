054de6515435b93fc7eb0cf0b74df9032f6b4076
omukombiti <mwirigiomukombiti@gmail.com>
2023-10-02 11:52:56 -0700

    0x08p

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+"""
+Module 0-Rectangle
+Contains a class Rectangle
+Makes an empty Rectangle class
+"""
 
-"""Defines a Rectangle class."""
 
+class Rectangle(object):
+        """
+            This class creates an instance of a Rectangle class
+                It creates an empty Rectangle object
+                    """
 
-class Rectangle:
-        """Represent a rectangle."""
-            pass
+                        pass
